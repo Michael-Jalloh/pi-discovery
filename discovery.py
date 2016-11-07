18a717631d230e5377ecd88c0e921beae8223846
Michael Jalloh <michaeljalloh19@gmail.com>
2016-11-07 12:28:13 +0000

    Server Program

---
+++ discovery.py
@@ -0,0 +1,16 @@
+from socket import *
+
+server = socket(AF_INET, SOCK_DGRAM)
+server.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+server.bind(('0.0.0.0',9000))
+while 1:
+	try:
+		data, addr = server.recvfrom(1024)
+		if data == 'raspberrypi':
+			server.sendto('raspberrypi', addr)
+			break
+		else:
+			print 'Wrong client'
+	except:
+		break
+
